ae8332a0ef759bd8b8f94ac20321c9c4f0fb105b
valor321 <o.brightey@gmail.com>
2024-12-17 17:41:55 +0000

    added comments to the code

--- page_rank.py
+++ page_rank.py
@@ -7,14 +7,6 @@ import random
 
 def load_graph(filename):
 
-    """Load graph from text file
-
-    Parameters:
-    args -- arguments named tuple
-
-    Returns:
-    A dict mapling a URL (str) to a list of target URLs (str).
-    """
     graphL = {}
 
     with open(filename, "r") as f:
@@ -25,13 +17,14 @@ def load_graph(filename):
             if len(node)  == 2:
                 url = node[0].strip()
                 target = (node[1].strip())
+                #check if the url is already in the dic and assigns another value to it
                 if url in graphL:
                     graphL[url].append(target)
                 else:
+                    #otherwise created a new dic value
                     graphL[url] = [target]
 
     return graphL
-            #raise RuntimeError("This function is not implemented yet.")
 
 
 def print_stats(graphL):
@@ -39,23 +32,10 @@ def print_stats(graphL):
         num_of_edges = sum(len(targets) for targets in graphL.values())
         print("Number of nodes:", num_nodes)
         print("Number of edges:", num_of_edges)
-        #raise RuntimeError("This function is not implemented yet.")
 
 
 def stochastic_page_rank(graphP, repeats):
-    """Stochastic PageRank estimation
 
-    Parameters:
-    graph -- a graph object as returned by load_graph()
-    args -- arguments named tuple
-
-    Returns:
-    A dict that assigns each page its hit frequency
-
-    This function estimates the Page Rank by counting how frequently
-    a random walk that starts on a random node will after n_steps end
-    on each node of the given graph.
-    """
     #how many times a node is visited
     hit_count = {node: 0 for node in graphP}
 
@@ -65,18 +45,19 @@ def stochastic_page_rank(graphP, repeats):
 
     for _ in range(repeats):
         if not graphP[current_node]:
+            #if they dont have edges, choose another node
             current_node = random.choice(list(graphP.keys()))
         else:
+            #seeing if they have edges
             current_node = random.choice(graphP[current_node])
 
         hit_count[current_node] += 1
 
     return  hit_count
 
-    #raise RuntimeError("This function is not implemented yet.")
 
 
-def distribution_page_rank(graph, args):
+def distribution_page_rank(graphD, steps):
     """Probabilistic PageRank estimation
 
     Parameters:
@@ -89,6 +70,23 @@ def distribution_page_rank(graph, args):
     This function estimates the Page Rank by iteratively calculating
     the probability that a random walker is currently on any node.
     """
+    #prob. of all nodes balanced
+    node_prob = {node: 1/len(graphD) for node in graphD}
+
+    for _ in range(steps):
+        #all nodes prob. to 0
+        next_prob = {node: 0 for node in graphD}
+        #update all nodes prob
+        for node in graphD:
+            p = node_prob[node] / len(graphD) if graphD[node] else 0
+            #distrubute probs. of all edges
+            for target in graphD[node]:
+                next_prob[target] += p
+
+            node_prob[node] = next_prob[node]
+
+    return node_prob
+
     #raise RuntimeError("This function is not implemented yet.")
 
 
@@ -110,11 +108,10 @@ if __name__ == '__main__':
 
 
     args = parser.parse_args()
-    #algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
+    algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
 
     graph = load_graph("school_web2024-1.txt")
     print_stats(graph)
-    print((stochastic_page_rank(graph, 1_000_000)))
 
     #start = time.time()
     #ranking = algorithm(graph, args)
