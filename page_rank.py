c0669b7538911384c883a6bded4cb710984678b5
valor321 <o.brightey@gmail.com>
2024-12-17 17:27:17 +0000

    got the load_graph working

--- page_rank.py
+++ page_rank.py
@@ -2,10 +2,10 @@ import sys
 import os
 import time
 import argparse
-from progress import Progress
+import random
 
 
-def load_graph(file):
+def load_graph(filename):
 
     """Load graph from text file
 
@@ -15,27 +15,34 @@ def load_graph(file):
     Returns:
     A dict mapling a URL (str) to a list of target URLs (str).
     """
-    graph = {}
+    graphL = {}
 
-    with open(file, "r") as f:
+    with open(filename, "r") as f:
         # Iterate through the file line by line
         for line in f:
             # And split each line into two URLs
-            node = line.split()
-            if len(node) >= 1:
-                url = node[0]
-                target = node[1:]
-                graph[url] = target
-    return graph
+            node = line.strip().split()
+            if len(node)  == 2:
+                url = node[0].strip()
+                target = (node[1].strip())
+                if url in graphL:
+                    graphL[url].append(target)
+                else:
+                    graphL[url] = [target]
+
+    return graphL
             #raise RuntimeError("This function is not implemented yet.")
 
 
-def print_stats(graph):
-        """Print number of nodes and edges in the given graph"""
-        raise RuntimeError("This function is not implemented yet.")
+def print_stats(graphL):
+        num_nodes = len(graphL)
+        num_of_edges = sum(len(targets) for targets in graphL.values())
+        print("Number of nodes:", num_nodes)
+        print("Number of edges:", num_of_edges)
+        #raise RuntimeError("This function is not implemented yet.")
 
 
-def stochastic_page_rank(graph, args):
+def stochastic_page_rank(graphP, repeats):
     """Stochastic PageRank estimation
 
     Parameters:
@@ -49,7 +56,24 @@ def stochastic_page_rank(graph, args):
     a random walk that starts on a random node will after n_steps end
     on each node of the given graph.
     """
-    raise RuntimeError("This function is not implemented yet.")
+    #how many times a node is visited
+    hit_count = {node: 0 for node in graphP}
+
+    current_node = random.choice(list(graphP.keys()))
+
+    hit_count[current_node] += 1
+
+    for _ in range(repeats):
+        if not graphP[current_node]:
+            current_node = random.choice(list(graphP.keys()))
+        else:
+            current_node = random.choice(graphP[current_node])
+
+        hit_count[current_node] += 1
+
+    return  hit_count
+
+    #raise RuntimeError("This function is not implemented yet.")
 
 
 def distribution_page_rank(graph, args):
@@ -65,7 +89,7 @@ def distribution_page_rank(graph, args):
     This function estimates the Page Rank by iteratively calculating
     the probability that a random walker is currently on any node.
     """
-    raise RuntimeError("This function is not implemented yet.")
+    #raise RuntimeError("This function is not implemented yet.")
 
 
 parser = argparse.ArgumentParser(description="Estimates page ranks from link information")
@@ -80,23 +104,25 @@ parser.add_argument('-n', '--number', type=int, default=20, help="number of resu
 
 if __name__ == '__main__':
 
-    print(load_graph(file="school_web2024-1.txt"))
+    #graph = load_graph("school_web2024-1.txt")
+    #print_stats(graph)
 
-    '''
+
+
     args = parser.parse_args()
-    algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
+    #algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
 
     graph = load_graph("school_web2024-1.txt")
-
     print_stats(graph)
+    print((stochastic_page_rank(graph, 1_000_000)))
 
-    start = time.time()
-    ranking = algorithm(graph, args)
-    stop = time.time()
-    time = stop - start
+    #start = time.time()
+    #ranking = algorithm(graph, args)
+    #stop = time.time()
+    #time = stop - start
 
-    top = sorted(ranking.items(), key=lambda item: item[1], reverse=True)
-    sys.stderr.write(f"Top {args.number} pages:\n")
-    print('\n'.join(f'{100*v:.2f}\t{k}' for k,v in top[:args.number]))
-    sys.stderr.write(f"Calculation took {time:.2f} seconds.\n")
-    '''
+    #top = sorted(ranking.items(), key=lambda item: item[1], reverse=True)
+    #sys.stderr.write(f"Top {args.number} pages:\n")
+    #print('\n'.join(f'{100*v:.2f}\t{k}' for k,v in top[:args.number]))
+    #sys.stderr.write(f"Calculation took {time:.2f} seconds.\n")
+    #'''
